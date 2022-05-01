8e2f5aa0285c789ceed0e76de6d784690eb6fc50
Pasha Zubak <pashazubak@gmail.com>
2022-05-02 00:15:31 +0300

    create slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 const el = document.querySelectorAll('img');
 const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
 observer.observe();
+
+const swiper = new Swiper('#projects-slider', {
+  loop: true,
+  effect: "fade",
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
